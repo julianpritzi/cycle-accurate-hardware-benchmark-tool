0e5fee78d7278e4234ce54e5e1052e521684a483
Julian <julian.pritzi@gmail.com>
2022-04-12 16:53:49 +0200

    added a simple global allocator

--- suite/src/main.rs
+++ suite/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
+#![feature(default_alloc_error_handler)]
 #![test_runner(crate::runtime::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -9,11 +10,19 @@ mod platform;
 #[macro_use]
 mod runtime;
 
+use alloc::string::String;
 use platform::Platform;
 use riscv_rt::entry;
 
+extern crate alloc;
+
 fn main() {
     println!("Hello World!");
+
+    let s = String::from("Hello allocated World!");
+    for sub_str in s.split_whitespace() {
+        println!("{}", sub_str)
+    }
 }
 
 #[entry]
--- suite/src/runtime.rs
+++ suite/src/runtime.rs
@@ -1,18 +1,66 @@
 //! Contains functions and macros for providing a runtime environment to the bechmarking suite
-use core::panic::PanicInfo;
+use core::{alloc::GlobalAlloc, cell::RefCell, panic::PanicInfo, ptr::NonNull};
+
+use linked_list_allocator::Heap;
 
 use crate::platform::{self, Platform};
 
-/// Initializes the communication module and enables the use of the print & println macro
+#[global_allocator]
+static ALLOCATOR: CustomHeap = CustomHeap::empty();
+
+extern "C" {
+    static _sheap: u8;
+    static _heap_size: u8;
+}
+
+/// Initializes the heap and enables use of the alloc crate,
+/// also initializes communication module and enables the use of the print & println macro
 ///
 /// # Safety
 ///  - only call once
 pub unsafe fn init() -> Result<(), &'static str> {
+    let heap_bottom = &_sheap as *const u8 as usize;
+    let heap_size = &_heap_size as *const u8 as usize;
+    ALLOCATOR.init(heap_bottom, heap_size);
+
     // Safety:
     // This should be the first time the communication module is accessed,
     // invalidating previous references is ok
     let comm = platform::current().get_communication_module();
     comm.init()
+}
+
+/// Since the architecture is assumed to be on a single core and without atomic instructions
+/// the GlobalAlloc Trait has to be manually implemented for Heap, therefore we define this
+/// Wrapper type
+struct CustomHeap(RefCell<Heap>);
+
+impl CustomHeap {
+    const fn empty() -> CustomHeap {
+        CustomHeap(RefCell::new(Heap::empty()))
+    }
+
+    unsafe fn init(&self, heap_bottom: usize, heap_size: usize) {
+        self.0.borrow_mut().init(heap_bottom, heap_size)
+    }
+}
+
+unsafe impl Sync for CustomHeap {}
+
+unsafe impl GlobalAlloc for CustomHeap {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        self.0
+            .borrow_mut()
+            .allocate_first_fit(layout)
+            .ok()
+            .map_or(0 as *mut u8, |addr| addr.as_ptr())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        self.0
+            .borrow_mut()
+            .deallocate(NonNull::new_unchecked(ptr), layout)
+    }
 }
 
 // Safety of calling get_communication_module() inside the macros:
@@ -97,5 +145,17 @@ mod tests {
     #[test_case]
     fn tests_are_working() {
         assert_eq!(1, 1);
+    }
+
+    #[test_case]
+    fn basic_allocations_are_working() {
+        use alloc::collections::vec_deque::VecDeque;
+
+        let mut vector = VecDeque::new();
+        for i in 0..10 {
+            vector.push_back(i);
+        }
+
+        assert_eq!(vector.len(), 10)
     }
 }
